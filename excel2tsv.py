9c0698f1914e3bc15ad5308f8ec0b45dd06a86b0
Paulo Suderio <paulo.suderio@gmail.com>
2026-07-17 17:07:05 -0300

    feat: database refactoring

--- excel2tsv.py
+++ excel2tsv.py
@@ -5,4 +5,6 @@ import sys
 
 # Usage: ./csv2tsv.py < input.csv > output.tsv
 if __name__ == "__main__":
-    csv.writer(sys.stdout, dialect="excel-tab").writerows(csv.reader(sys.stdin))
+    csv.writer(sys.stdout, dialect="excel-tab").writerows(
+        csv.reader(sys.stdin, delimiter=";")
+    )
